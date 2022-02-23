209c12305de7c0ed8332808dfb2bee9ddf576a8e
Justin Merz <jrmerz@gmail.com>
2022-02-23 11:54:51 -0800

    hacking in react based html to gutenberg block conversion

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+import "./lib/browser.js"; // must be first import!
 import wp from "./lib/wp.js";
-import {PostTransform} from "./lib/transform/index.js"
+import {PostTransform} from "./lib/transform/index.js";
 
 (async function() {
-  let transform = new PostTransform(await wp.getPost('55532'));
-  transform.run();
 
+  let transform = new PostTransform(await wp.get('post', '55532'));
+  await transform.run();
+  // console.log(transform.newPost);
+
+  process.exit();
 })();
