7390b28083d8c1fd326ed11a02319a7f1448a7c5
Justin Merz <jrmerz@gmail.com>
2022-02-25 09:57:17 -0800

    adding ucd flex content transform

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import {PostTransform} from "./lib/transform/index.js";
 (async function() {
 
   // let transform = new PostTransform(await wp.get('post', '55532'));
-  let transform = new PostTransform(await wp.get('post', '55174'));
+  let transform = new PostTransform(await wp.get('exhibit', '14241'));
   await transform.run();
 
   process.exit();
