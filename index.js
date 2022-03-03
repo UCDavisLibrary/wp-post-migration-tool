dfa97d11e0bb69d7af520201d1afbc9a4e819a49
Justin Merz <jrmerz@gmail.com>
2022-03-03 15:02:46 -0800

    adding tooling

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 import "./lib/browser.js"; // must be first import!
 import wp from "./lib/wp.js";
+import config from "./lib/config.js"
 import {PostTransform} from "./lib/transform/index.js";
+import errors from "./lib/errors.js";
 
 (async function() {
 
-  // let transform = new PostTransform(await wp.get('post', '55532'));
-  let transform = new PostTransform(await wp.get('exhibit', '14241'));
+  let transform = new PostTransform(await wp.get('exhibit', '52800'));
   await transform.run();
 
   process.exit();
 })();
+
+
+// const TYPE = 'posts';
+const TYPE = 'exhibit';
+
+async function allPosts() {
+  let page = 1;
+
+  let {host, apiPath} = config.source;
+  while( 1 ) {
+    let list = await wp.fetchJson(host+apiPath+'/'+TYPE+'?per_page=100&page='+page, true, false);
+    
+    for( let result of list ) {
+      try {
+        let transform = new PostTransform(result);
+        await transform.run({skipExisting: false});
+      } catch(e) {
+        console.error('Failed to migrate post: '+result.id);
+        console.error(e);
+        errors.handle(result, {
+          error : {
+            message : e.message,
+            stack : e.stack
+          },
+          description: 'Failed to migrate post: '+result.id
+        });
+      }
+    }
+
+    if( list.length < 100 ){
+      break;
+    }
+
+    page++;
+  }
+
+  process.exit();
+}
+
+// allPosts();
