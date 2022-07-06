c9aafcc88bf7768857964efea5fd5dd2966d8be8
Justin Merz <jrmerz@gmail.com>
2022-07-06 14:50:53 -0700

    adding exhibit transform

---
+++ cli/allExhibits.js
@@ -0,0 +1,46 @@
+import "../lib/browser.js"; // must be first import!
+import wp from "../lib/wp.js";
+import config from "../lib/config.js"
+import {ExhibitTransform} from "../lib/transform/exhibit.js";
+import errors from "../lib/errors.js";
+
+const TYPE = 'exhibit';
+
+async function allPosts() {
+  let page = 1;
+
+  console.log('Migrating from '+config.source.host+' to '+config.sink.host);
+
+  let {host, apiPath} = config.source;
+  while( 1 ) {
+    let list = await wp.fetchJson(host+apiPath+'/'+TYPE+'?per_page=100&page='+page, true, false);
+    
+    for( let result of list ) {
+      try {
+        let transform = new ExhibitTransform(result);
+        await transform.run({skipExisting: false});
+      } catch(e) {
+        console.error('Failed to migrate post: '+result.id);
+        console.error(e);
+        errors.handle(result, {
+          error : {
+            message : e.message,
+            stack : e.stack
+          },
+          description: 'Failed to migrate post: '+result.id
+        });
+      }
+
+    }
+
+    if( list.length < 100 ){
+      break;
+    }
+
+    page++;
+  }
+
+  process.exit();
+}
+
+allPosts();
